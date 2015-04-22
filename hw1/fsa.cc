f0a0fa224daae9b77c1a7a84a4f74f0e0b954b3d
Hyunjun Kim <yoloseem@gmail.com>
2015-04-22 22:28:52 +0900

    Add whitespace between opening brace and "for"

--- hw1/fsa.cc
+++ hw1/fsa.cc
@@ -16,7 +16,7 @@ using namespace std;
 bool CheckIfNFA(const TableElement* elements, int num_elements) {
   set< pair<int, char> > sMovesOnStates;
 
-  for(int i=0; i<num_elements; i++) {
+  for (int i=0; i<num_elements; i++) {
     if (elements[i].input_char == kEps) {
       // If there exists any epsilon-moves, it is NFA
       LOG << "Epsilon-move found on state " << elements[i].state << endl;
