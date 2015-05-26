14c7a1370209c7c729b1ad22b8c4d18c9540a41c
Hyunjun Kim <yoloseem@gmail.com>
2015-05-26 21:17:56 +0900

    Implemented parser part of BuildRegExpMatcher

--- hw2/regexp_matcher.cc
+++ hw2/regexp_matcher.cc
@@ -34,9 +34,7 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
   char handle;
   RegExp* currentRegExp = rootRegExp;
   while((handle = regexp[cursor]) != '\0') {
-      printf("handle: %c ", handle);
       if (handle == ANYCHAR) {
-          printf("Any single character\n");
           // 1. construct new regexp accepts any single character
           RegExp* anyCharRegExp = new RegExp(RE_ANYCHAR);
           // 2. and push it into current stack
@@ -45,7 +43,6 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
           cursor++;
       }
       else if (handle == STAR) {
-          printf("Kleene-Star\n");
           // If no previous element to *-repeat, error
           if (currentRegExp->elements[0].size() == 0) return false;
           // 1. construct new *-repeat regexp on previous element
@@ -59,7 +56,6 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
           cursor++;
       }
       else if (handle == OPEN_GROUP) {
-          printf("Opening new group of expressions\n");
           // 1. construct new group
           RegExp* groupRegExp = new RegExp(RE_GROUP);
           groupRegExp->container = currentRegExp;
@@ -71,14 +67,12 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
           cursor++;
       }
       else if (handle == CLOSE_GROUP) {
-          printf("Closing group of expressions\n");
           // Closing unopened group, error
           if (currentRegExp->tokenType != RE_GROUP) return false;
           currentRegExp = currentRegExp->container;
           cursor++;
       }
       else if (handle == OPEN_SET) {
-          printf("Opening new set of characters\n");
           // 1. construct new set
           RegExp* setRegExp = new RegExp(RE_SETCHAR);
           setRegExp->container = currentRegExp;
@@ -90,20 +84,17 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
           cursor++;
       }
       else if (handle == CLOSE_SET) {
-          printf("Closing set of characters\n");
           // Closing unopened set, error
           if (currentRegExp->tokenType != RE_SETCHAR) return false;
           currentRegExp = currentRegExp->container;
           cursor++;
       }
       else if (handle == OR) {
-          printf("OR expression\n");
           currentRegExp->or_ops_count++;
           currentRegExp->elements.push_back(vector<RegExp*>(0));
           cursor++;
       }
       else /* alphabets */ {
-          printf("Single character\n");
           // 1. construct new regexp accepts any single character
           RegExp* singleCharRegExp = new RegExp(RE_CHAR);
           singleCharRegExp->primitiveValue = handle;
@@ -114,10 +105,7 @@ bool BuildRegExpMatcher(const char* regexp, RegExpMatcher* regexp_matcher) {
       }
   }
 
-  printRegExp(rootRegExp);
-  printf("\n");
-
-  return false;
+  return true;
 }
 
 bool RunRegExpMatcher(const RegExpMatcher* regexp_matcher, const char* str) {
