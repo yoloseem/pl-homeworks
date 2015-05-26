7aeff7a749eae85d17f63403b206d094aa1fc4ae
Hyunjun Kim <yoloseem@gmail.com>
2015-05-26 20:19:48 +0900

    Define struct RegExp for storing parsed regexp tokens

--- hw2/regexp_matcher.h
+++ hw2/regexp_matcher.h
@@ -1,8 +1,35 @@
 // PL homework: hw2
 // regexp_matcher.h
 
+#include <vector>
+using namespace std;
+
 #ifndef _PL_HOMEWORK_REGEXP_MATCHER_H_
 #define _PL_HOMEWORK_REGEXP_MATCHER_H_
+
+enum RegExpTokenType {
+    RE_REGEXP,  // Sequence of any regular expressions
+    RE_CHAR,  // Single character
+    RE_ANYCHAR,  // Accepts any character
+    RE_SEQCHAR,  // Sequence of single characters
+    RE_SETCHAR,  // Set of single characters
+    RE_GROUP,  // Similar to REGEXP but has delimiting braces
+    RE_OR,  // OR-operation on two regular expressions
+    RE_STAR,  // 0-or-more repetitions of single regular expression
+};
+const char ANYCHAR = '.';
+const char OPEN_GROUP = '(';
+const char CLOSE_GROUP = ')';
+const char OPEN_SET = '[';
+const char CLOSE_SET = ']';
+const char STAR = '*';
+const char OR = '|';
+
+struct RegExp {
+  RegExpTokenType tokenType;
+  char primitiveValue;  // Used only when tokenType is CHARACTER
+  vector<RegExp> elements;  // Children elements
+};
 
 struct RegExpMatcher {
   // Design your RegExpMatcher structure.
